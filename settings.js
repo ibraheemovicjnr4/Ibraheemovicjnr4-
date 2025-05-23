02eaa079d743a7ea72507c01573063e0ed61b781
ibraheemovicjnr4 <ibraheemariyo4190@gmail.com>
2025-05-23 15:43:39 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -7,9 +7,9 @@ global.socialm = "GitHub: Elite-MD" //ur github or insta name
 global.location = "Port Harcourt, Nigeria" //ur location
 
 //new settings 
-global.botname = 'ELITE-PRO-V2' //ur bot name
-global.ownernumber = '2348109263390' //ur owner number
-global.ownername = 'ELITE-PRO-V2' //ur owner name
+global.botname = '' //ur bot name
+global.ownernumber = '2349137139054' //ur owner number
+global.ownername = 'ibraheem' //ur owner name
 global.websitex = "https://youtube.com/@elitepro-empire"
 global.elitepropp = 'https://i.ibb.co/jk0ynvbn/7a8c4b5b617fa11a1e9a61190f427546.jpg' // Change this image link to yours.
 
@@ -17,7 +17,7 @@ global.themeemoji = '👨🏼‍💻'
 global.wm = "ELITE-PRO-V2"
 global.botscript = 'https://github.com/Elite-MD/Elite-Pro-V2' //script link
 global.packname = "Sticker By"
-global.author = "ELITE-PRO-V2\n\n+2348109263390"
+global.author = "ELITE-PRO-V2\n\n+2349137139054"
 global.creator = "2348109263390@s.whatsapp.net"
 global.xprefix = '.' //set any prefix 
 global.premium = ["2348109263390"] // Premium User
